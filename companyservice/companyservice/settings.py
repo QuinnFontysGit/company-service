6dbd921c327e2d06a633d0df7fccf6ddffd58d72
Quinn Aelen <442982@student.fontys.nl>
2024-06-21 20:36:54 +0200

    added kubernetes allowed host

--- companyservice/companyservice/settings.py
+++ companyservice/companyservice/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['companybackend', 'localhost']
+ALLOWED_HOSTS = ['companybackend', 'localhost', 'companybackend.default.svc.cluster.local']
 
 # Application definition
 
